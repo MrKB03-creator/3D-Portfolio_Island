308f71024e6feab1c43bd6aea38a3c0b75555c58
MrKB03-creator <badillokurt9@gmail.com>
2025-03-10 18:24:29 -0700

    Trying to fish vite.config.js

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/3D-Portfolio_Island",
+  base: "/3D-Portfolio_Island/",
   plugins: [react()],
   assetsInclude: ['**/*.glb'],
 
