2abbcaa8e85b23882caed4d6afb84091eded9189
MrKB03-creator <badillokurt9@gmail.com>
2025-03-10 18:11:11 -0700

    adding gp pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  assetsInclude: ['**/*.glb']
+  assetsInclude: ['**/*.glb'],
+  base: "/3D-Portfolio_Island",
+
 })
